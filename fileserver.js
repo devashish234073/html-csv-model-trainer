5cad8e31674696cb61e97cdca023f6a5bc96b674
Devashish Priyadarshi <devashish234073@gmail.com>
2023-06-08 20:15:49 +0530

    added model selection from server to predict new values

--- fileserver.js
+++ fileserver.js
@@ -9,7 +9,7 @@ var server = http.createServer((req,res)=>{
 	if(req.url.indexOf("/api/sendModel")==0) {
 		const form = new multiparty.Form();
 		const urlObj = url.parse(req.url, true);
-		console.log(urlObj.query.modelName);
+		let modelName = urlObj.query.modelName;
 		form.parse(req, (err, fields, files) => {
 			if (err) {
 			  console.error('Error parsing form:', err);
@@ -17,14 +17,20 @@ var server = http.createServer((req,res)=>{
 			  res.end();
 			  return;
 			}
+			if (!fs.existsSync("models")) {
+			  fs.mkdirSync("models");
+			}
+			if (!fs.existsSync("models/"+modelName)) {
+			  fs.mkdirSync("models/"+modelName);
+			}
 			const modelJSONFile = files['model.json'][0];
-			const modelJSONPath = path.join(__dirname, 'models/model.json');
+			const modelJSONPath = path.join(__dirname, 'models/'+modelName+'/model.json');
 			console.log("reading from "+modelJSONFile.path);
 			const modelJSONData = fs.readFileSync(modelJSONFile.path);
 			fs.writeFileSync(modelJSONPath, modelJSONData);
 	  
 			const modelWeightsFile = files['model.weights.bin'][0];
-			const modelWeightsPath = path.join(__dirname, 'models/model.weights.bin');
+			const modelWeightsPath = path.join(__dirname, 'models/'+modelName+'/model.weights.bin');
 			console.log("reading from "+modelWeightsFile.path);
 			const modelWeightsData = fs.readFileSync(modelWeightsFile.path);
 			fs.writeFileSync(modelWeightsPath, modelWeightsData);
@@ -33,8 +39,12 @@ var server = http.createServer((req,res)=>{
 			res.end();
 		  });
 	} else if(req.url.indexOf("/api/getModel")==0) {
-		let fileName = String(req.url).replace("/api/getModel","");
-		fs.readFile("models/"+fileName,(err,data)=>{
+		let urlSplit = String(req.url).split("/");
+		let modelName = urlSplit[urlSplit.length-2];
+		let fileName = urlSplit[urlSplit.length-1];
+		let filePath = "models/"+modelName+"/"+fileName;
+		console.log("reading "+filePath);
+		fs.readFile(filePath,(err,data)=>{
 			if(err) {
 				res.end(err);
 			} else {
@@ -46,7 +56,16 @@ var server = http.createServer((req,res)=>{
 			if(err) {
 				res.end(err);
 			} else {
-				res.end(data);
+				let html = String(data);
+				let modeloptions = "<option value=''>Choose Model</option>";
+				if (fs.existsSync("models")) { 
+					let models = fs.readdirSync("models");
+					for(let i in models) {
+						modeloptions+="<option value='"+models[i]+"'>"+models[i]+"</option>";
+					}
+				} 
+				html = html.replace("__MODELS__",modeloptions);
+				res.end(html);
 			}
 		});
 	}
